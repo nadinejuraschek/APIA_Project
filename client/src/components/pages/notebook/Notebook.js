0e44dac6b1d0a50824e9f20c7337109ea6fca8ac
Nadine Ju <nadine.jura@online.de>
2020-03-03 19:31:04 -0800

    stylechanges

--- client/src/components/pages/notebook/Notebook.js
+++ client/src/components/pages/notebook/Notebook.js
@@ -4,30 +4,34 @@ const Notebook = () => {
     return (
         <section className="hero is-fullheight-with-navbar is-bold">
             <div className="container">
-                <div className="columns">
-                    <div className="column">
-                    <a href="/notebook/workhours">
-                        <div className="card notebook-card">
-                        My Work Hours
-                        </div>
-                    </a>
-                    <a href="/notebook/goals">
-                        <div className="card notebook-card">
-                        My Goals
-                        </div>
-                    </a>
+                <div className="columns is-multiline">
+                    <div className="column is-half">
+                        <a href="/notebook/workhours">
+                            <div className="card notebook-card">
+                            My Work Hours
+                            </div>
+                        </a>
                     </div>
-                    <div className="column">
-                    <a href="/notebook/payments">
-                        <div className="card notebook-card">
-                        My Payments
-                        </div>
-                    </a>
-                    <a href="/notebook/notes">
-                        <div className="card notebook-card">
-                        My Notes
-                        </div>
-                    </a>
+                    <div className="column is-half">
+                        <a href="/notebook/goals">
+                            <div className="card notebook-card">
+                            My Goals
+                            </div>
+                        </a>
+                    </div>
+                    <div className="column is-half">
+                        <a href="/notebook/payments">
+                            <div className="card notebook-card">
+                            My Payments
+                            </div>
+                        </a>
+                    </div>
+                    <div className="column is-half">
+                        <a href="/notebook/notes">
+                            <div className="card notebook-card">
+                            My Notes
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
