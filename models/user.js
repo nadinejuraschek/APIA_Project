9aeccdbd2dcdc38890750529964a8c07bf75b844
Nadine Ju <nadine.jura@online.de>
2020-03-05 20:36:19 -0800

    setting up backend server.js

---
+++ models/user.js
@@ -0,0 +1,13 @@
+// NPM PACKAGES
+const   mongoose                = require('mongoose'),
+        passportLocalMongoose   = require('passport-local-mongoose');
+
+// SCHEMA SETUP
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 // NPM PACKAGES
 require('dotenv').config();
 
-const   express     = require('express'),
-        app         = express(),
-        mongoose    = require('mongoose');
+const   express         = require('express'),
+        app             = express(),
+        mongoose        = require('mongoose'),
+        passport        = require('passport'),
+        LocalStrategy   = require('passport-local'),
+        User            = require('./models/user');
 
+// DATABASE
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// PASSPORT CONFIG
+app.use(require('express-session')({
+    secret: process.env.APP_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// SERVER
 app.listen(process.env.PORT, function() {
     console.log('Server is listening on PORT ' + process.env.PORT + '.');
 });
