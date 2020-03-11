2c635fd877bba8e18001ec2f9cfa9da68c666a9f
Nadine Ju <nadine.jura@online.de>
2020-03-11 10:26:48 -0700

    workhour model created

---
+++ client/src/contexts/WorkHourContext.js
@@ -0,0 +1,200 @@
+import React, { createContext } from 'react';
+
+export const WorkHourContext = createContext();
+
+export const WorkHourProvider = props => {
+    const [ workhours, setWorkhours ] = useState([
+        {
+            week: {
+                number: 1,
+                day: [
+                    {
+                        date: 02/03/2020,
+                        time: [
+                            {
+                                start: '6:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 8
+                            }
+                        ],
+                        dailyHours: 8,
+                        dayOff: false
+                    },
+                    {
+                        date: 02/04/2020,
+                        time: [
+                            {
+                                start: '6:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 8
+                            }
+                        ],
+                        dailyHours: 8,
+                        dayOff: false
+                    },{
+                        date: 02/05/2020,
+                        time: [
+                            {
+                                start: '9:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 5
+                            },
+                            {
+                                start: '6:00 AM',
+                                end: '8:00 PM',
+                                totalHours: 2
+                            }
+                        ],
+                        dailyHours: 7,
+                        dayOff: false
+                    },{
+                        date: 02/06/2020,
+                        time: [
+                            {
+                                start: '6:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 8
+                            }
+                        ],
+                        dailyHours: 8,
+                        dayOff: false
+                    },{
+                        date: 02/07/2020,
+                        time: [
+                            {
+                                start: '9:00 AM',
+                                end: '7:00 PM',
+                                totalHours: 10
+                            }
+                        ],
+                        dailyHours: 10,
+                        dayOff: false
+                    },
+                    {
+                        date: 02/08/2020,
+                        time: [
+                            {
+                                start: '',
+                                end: '',
+                                totalHours: null
+                            }
+                        ],
+                        dailyHours: null,
+                        dayOff: true
+                    },
+                    {
+                        date: 02/09/2020,
+                        time: [
+                            {
+                                start: '',
+                                end: '',
+                                totalHours: null
+                            }
+                        ],
+                        dailyHours: null,
+                        dayOff: true
+                    }
+                ],
+                weeklyHours: 43  
+            },
+            week: {
+                number: 2,
+                day: [
+                    {
+                        date: 02/10/2020,
+                        time: [
+                            {
+                                start: '6:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 8
+                            }
+                        ],
+                        dailyHours: 8,
+                        dayOff: false
+                    },
+                    {
+                        date: 02/11/2020,
+                        time: [
+                            {
+                                start: '6:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 8
+                            }
+                        ],
+                        dailyHours: 8,
+                        dayOff: false
+                    },{
+                        date: 02/12/2020,
+                        time: [
+                            {
+                                start: '9:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 5
+                            },
+                            {
+                                start: '4:00 AM',
+                                end: '10:00 PM',
+                                totalHours: 6
+                            }
+                        ],
+                        dailyHours: 11,
+                        dayOff: false
+                    },{
+                        date: 02/13/2020,
+                        time: [
+                            {
+                                start: '6:00 AM',
+                                end: '2:00 PM',
+                                totalHours: 8
+                            }
+                        ],
+                        dailyHours: 8,
+                        dayOff: false
+                    },{
+                        date: 02/14/2020,
+                        time: [
+                            {
+                                start: '9:00 AM',
+                                end: '7:00 PM',
+                                totalHours: 10
+                            }
+                        ],
+                        dailyHours: 10,
+                        dayOff: false
+                    },
+                    {
+                        date: 02/15/2020,
+                        time: [
+                            {
+                                start: '8:00 PM',
+                                end: '10:00 PM',
+                                totalHours: 2
+                            }
+                        ],
+                        dailyHours: 2,
+                        dayOff: false
+                    },
+                    {
+                        date: 02/16/2020,
+                        time: [
+                            {
+                                start: '',
+                                end: '',
+                                totalHours: null
+                            }
+                        ],
+                        dailyHours: null,
+                        dayOff: true
+                    }
+                ],
+                weeklyHours: 43  
+            }
+        }
+    ]);
+
+    return (
+        <WorkHourContext.Provider value={workhours}>
+            {this.children}
+        </WorkHourContext.Provider>
+    )
+};
--- models/workHour.js
+++ models/workHour.js
@@ -3,18 +3,24 @@ const   mongoose = require('mongoose');
 
 // SCHEMA SETUP
 const workSchema = new mongoose.Schema({
-    week: Number,
-    date: Date,
-    time: [
-        {
-            start: Number,
-            end: Number,
-            totalHours: Number
-        }
-    ],
-    dailyHours: Number,
-    weeklyHours: Number,
-    dayOff: Boolean
+    week: {
+        number: Number,
+        day: [
+            {
+                date: String,
+                time: [
+                    {
+                        start: String,
+                        end: String,
+                        totalHours: Number
+                    }
+                ],
+                dailyHours: Number,
+                dayOff: Boolean
+            }
+        ],
+        weeklyHours: Number  
+    }
 });
 
 module.exports = mongoose.model('WorkHour', workSchema);
