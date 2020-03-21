6041fda33ba1bdf1e74233a7626fcca767e3db4c
Nadine Ju <nadine.jura@online.de>
2020-03-21 13:38:17 -0700

    current year for copyright rendered dynamically

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import logo from '../images/embrace.svg';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="ui vertical footer segment">
             {/* <h2 className="ui center aligned icon header">
@@ -11,7 +13,7 @@ const Footer = () => {
                 <div className="centered">
                     <p className="copyright">
                         <i className="copyright outline icon"></i>
-                         2020
+                         {currentYear}
                         <a href="https://github.com/nadinejuraschek" className="footer-link"> Nadine Juraschek</a>
                     </p>
                     {/* <p className="copyright">
