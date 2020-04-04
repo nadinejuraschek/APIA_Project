e6d06fc21ee11ac1ef8b64c76a75ca12e01758e4
Nadine Ju <nadine.jura@online.de>
2020-04-03 19:24:20 -0700

    workhour fields can be left empty, add button margin fixed

--- client/src/components/pages/notebook/workhours/WorkHours.js
+++ client/src/components/pages/notebook/workhours/WorkHours.js
@@ -19,7 +19,45 @@ const WorkHours = (props) => {
         show === true ? setShow(false) : setShow(true);
     };
 
-    const [ newHours, setNewHours ] = useState({});
+    const [ newHours, setNewHours ] = useState({
+        number: 0,
+        mon: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        tue: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        wed: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        thu: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        fri: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        sat: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        sun: {
+            date: '',
+            dailyHours: 0,
+            dayOff: true
+        },
+        weeklyHours: 0
+    });
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -31,6 +69,45 @@ const WorkHours = (props) => {
         }).then(response => {
             console.log('WorkHours in DB: ' + response.data);
             getWorkhours();
+            setNewHours({
+                number: 0,
+                mon: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                tue: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                wed: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                thu: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                fri: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                sat: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                sun: {
+                    date: '',
+                    dailyHours: 0,
+                    dayOff: true
+                },
+                weeklyHours: 0
+            });
         }).catch(error => {
             console.log('Error: ' + error)
         });
@@ -71,25 +148,43 @@ const WorkHours = (props) => {
                         <div className="field">
                             <label>Week</label>
                             <input 
-                                name="week" 
+                                name="number" 
                                 type="number" 
                                 min="1" max="52" 
                                 placeholder="#" 
                                 onChange={handleChange} 
+                                value={newHours.number}
                             />
                         </div>
                         <div className="three fields">
                             <div className="field">
                                 <label>Monday</label>
-                                <input name="mon.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="mon.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange}
+                                    // value={newHours.mon.date}
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="mon.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="mon.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange}
+                                    // value={newHours.mon.dailyHours} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="mon.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="mon.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.mon.dayOff}
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -100,15 +195,32 @@ const WorkHours = (props) => {
                         <div className="three fields">
                             <div className="field">
                                 <label>Tuesday</label>
-                                <input name="tue.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="tue.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange} 
+                                    // value={newHours.tue.date}
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="tue.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="tue.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange}
+                                    // value={newHours.tue.dailyHours} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="tue.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="tue.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.tue.dayOff}
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -119,15 +231,32 @@ const WorkHours = (props) => {
                         <div className="three fields">
                             <div className="field">
                                 <label>Wednesday</label>
-                                <input name="wed.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="wed.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange} 
+                                    // value={newHours.wed.date}
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="wed.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="wed.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange}
+                                    // value={newHours.wed.dailyHours} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="wed.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="wed.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.wed.dayOff}
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -138,15 +267,32 @@ const WorkHours = (props) => {
                         <div className="three fields">
                             <div className="field">
                                 <label>Thursday</label>
-                                <input name="thu.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="thu.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange}
+                                    // value={newHours.thu.date} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="thu.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="thu.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange}
+                                    // value={newHours.thu.dailyHours}  
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="thu.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="thu.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.thu.dayOff} 
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -157,15 +303,32 @@ const WorkHours = (props) => {
                         <div className="three fields">
                             <div className="field">
                                 <label>Friday</label>
-                                <input name="fri.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="fri.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange} 
+                                    // value={newHours.fri.date} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="fri.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="fri.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange}
+                                    // value={newHours.fri.dailyHours}  
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="fri.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="fri.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.fri.dayOff} 
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -176,15 +339,32 @@ const WorkHours = (props) => {
                         <div className="three fields">
                             <div className="field">
                                 <label>Saturday</label>
-                                <input name="sat.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="sat.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange} 
+                                    // value={newHours.sat.date} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="sat.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="sat.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange} 
+                                    // value={newHours.sat.dailyHours} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="sat.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="sat.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.sat.dayOff} 
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -195,15 +375,32 @@ const WorkHours = (props) => {
                         <div className="three fields">
                             <div className="field">
                                 <label>Sunday</label>
-                                <input name="sun.date" type="text" placeholder="Date" onChange={handleChange} />
+                                <input 
+                                    name="sun.date" 
+                                    type="text" 
+                                    placeholder="Date" 
+                                    onChange={handleChange}
+                                    // value={newHours.sun.date} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Daily Hours</label>
-                                <input name="sun.dailyHours" type="number" placeholder="Daily Hours" onChange={handleChange} />
+                                <input 
+                                    name="sun.dailyHours" 
+                                    type="number" 
+                                    placeholder="Daily Hours" 
+                                    onChange={handleChange}
+                                    // value={newHours.sun.dailyHours} 
+                                />
                             </div>
                             <div className="field">
                                 <label>Day Off?</label>
-                                <select name="sun.dayOff" className="ui fluid dropdown" onChange={handleChange}>
+                                <select 
+                                    name="sun.dayOff" 
+                                    className="ui fluid dropdown" 
+                                    onChange={handleChange}
+                                    // value={newHours.sun.dayOff} 
+                                >
                                 <option></option>
                                 <option value="true">Yes</option>
                                 <option value="false">No</option>
@@ -211,8 +408,8 @@ const WorkHours = (props) => {
                             </div>
                         </div>
 
-                        <div className="centered">
-                        <button className="ui button button-container">Add</button>
+                        <div className="centered button-container">
+                        <button className="ui button">Add</button>
                         </div>
                     </form>
                 </div>
